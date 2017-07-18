1305729eac3c8343da53eb5d511d48b90c783e5b
kunalkalbande <dineshr@IDTP362>
2017-07-18 20:27:23 +0530

    Fix for defect 141

--- Servosms/App_Code/Sysitem/Classes/InventoyCLass.cs
+++ Servosms/App_Code/Sysitem/Classes/InventoyCLass.cs
@@ -4320,7 +4320,7 @@ namespace Servosms.Sysitem.Classes
 			SqlCmd.Parameters .Add("@Cash_Disc_Type",Cash_Disc_Type );
 			SqlCmd.Parameters .Add("@VAT_Amount",VAT_Amount);
 			SqlCmd.Parameters .Add("@ChallanNo",ChallanNo);
-			SqlCmd.Parameters .Add("@ChallanDate",ChallanDate);
+			SqlCmd.Parameters .Add("@ChallanDate",System.Convert.ToDateTime(ChallanDate));
 			SqlCmd.Parameters .Add("@Credit_Limit",Credit_Limit);
 			/*bhal*/SqlCmd.Parameters .Add("@schdiscount",schdiscount);
 			//SqlCmd.Parameters .Add("@foediscount",float.Parse(foediscount));
@@ -4577,7 +4577,7 @@ namespace Servosms.Sysitem.Classes
 			SqlCmd.Parameters .Add("@Entry_Time",Entry_Time);
 			SqlCmd.Parameters .Add("@Slip_No",Slip_No);
 			SqlCmd.Parameters .Add("@ChallanNo",ChallanNo);
-			SqlCmd.Parameters .Add("@ChallanDate",ChallanDate);
+			SqlCmd.Parameters .Add("@ChallanDate", System.Convert.ToDateTime(ChallanDate));
 			SqlCmd.Parameters .Add("@Cash_Discount",Cash_Discount );
 			SqlCmd.Parameters .Add("@Cash_Disc_Type",Cash_Disc_Type );
 			SqlCmd.Parameters .Add("@VAT_Amount",VAT_Amount);
